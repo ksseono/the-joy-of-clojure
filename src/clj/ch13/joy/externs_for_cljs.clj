bee447f39a4fb042fe3e4591b0d7f58f5a7a3071
Seonho Kim <ksseono@gmail.com>
2015-09-12 00:02:31 +0900

    section 13.2 completed

--- src/clj/ch13/joy/externs_for_cljs.clj
+++ src/clj/ch13/joy/externs_for_cljs.clj
@@ -2,7 +2,9 @@
   (:require [cljs.compiler :as comp]
             [cljs.analyzer :as ana]
             [clojure.walk :refer [prewalk]]
-            [clojure.pprint :refer [pprint]]))
+            [clojure.pprint :refer [pprint]]
+            [clojure.java.io :as io])
+  (:import (clojure.lang LineNumberingPushbackReader)))
 
 (def code-string "(defn hello [x] (js/alert (pr-str 'greetings x)))")
 (def code-data (read-string code-string))
@@ -16,5 +18,39 @@
           (select-keys x [:op :form :name :children])
           x))
       ast)))
+
+(defn read-file
+  "Read the contents of filename as a sequence of Clojure values."
+  [filename]
+  (let [eof (Object.)]
+    (with-open [reader (LineNumberingPushbackReader.
+                         (io/reader filename))]
+      (doall
+        (take-while #(not= % eof)
+          (repeatedly #(read reader false eof)))))))
+
+(defn file-ast
+  "Return the ClojureScript AST for the contents of filename. Tends to be large
+   and to contain cycles -- be careful printing at the REPL."
+  [filename]
+  (binding [ana/*cljs-ns* 'cljs.user
+            ana/*cljs-file* filename]
+    (mapv #(ana/analyze (ana/empty-env) %)
+      (read-file filename))))
+
+(comment
+  (count (file-ast "src/cljs/joy/music.cljs"))
+  (first (file-ast "src/cljs/joy/music.cljs")))
+
+(defn flatten-ast [ast]
+  (mapcat #(tree-seq :children :children %) ast))
+
+
+
+
+
+
+
+
 
 
--- src/cljs/joy/music.cljs
+++ src/cljs/joy/music.cljs
@@ -5,7 +5,7 @@
    <volume> in 50 milliseconds, then ramps back down to silent after
    <duration>"
   [ctx {:keys [volume delay duration]}]
-  (let [node (.createGainNode ctx)]
+  (let [node (.createGain ctx)]
     (doto (.-gain node)
       (.linearRampToValueAtTime 0 delay)
       (.linearRampToValueAtTime volume (+ delay 0.05))
@@ -18,8 +18,8 @@
   (let [node (.createOscillator ctx)]
     (set! (-> node .-frequency .-value) 440)
     (set! (-> node .-detune .-value) (- cent 900))
-    (.noteOn node delay)
-    (.noteOff node (+ delay duration))
+    (.start node delay)
+    (.stop node (+ delay duration))
     node))
 
 (defn connect-to
@@ -56,7 +56,48 @@
     (js/alert "Sorry, this browser doesn't seem to support AudioContext")))
 
 ;; test
-(play! woo [{:cent 1100, :duration 1, :delay 0.0, :volume 0.4}
-            {:cent 1400, :duration 1, :delay 0.2, :volume 0.4}
-            {:cent 1800, :duration 1, :delay 0.4, :volume 0.4}])
+(comment
+  (play! woo [{:cent 1100, :duration 1, :delay 0.0, :volume 0.4}
+              {:cent 1400, :duration 1, :delay 0.2, :volume 0.4}
+              {:cent 1800, :duration 1, :delay 0.4, :volume 0.4}]))
+  
+(defn pair-to-note
+  "Return a note map for the given tone and duration"
+  [[tone duration]]
+  {:cent (* 100 tone)
+   :duration duration
+   :volume 0.4})
+
+(defn consecutive-notes
+  "Take a sequence of note maps that have no :delay, and return them with correct :delays
+   so that they will play in the order given."
+  [notes]
+  (reductions (fn [{:keys [delay duration]} note]
+                (assoc note
+                  :delay (+ delay duration)))
+    notes))
+
+(defn notes [tone-pairs]
+  "Returns a sequence of note maps at moderate tempo for the given sequence of tone-pairs."
+  (let [bpm 360
+        bps (/ bpm 60)]
+    (->> tone-pairs
+      (map pair-to-note)
+      consecutive-notes
+      (map #(update-in % [:delay] / bps))
+      (map #(update-in % [:duration] / bps)))))
+
+(defn magical-theme
+  "A sequence of notes for a magical theme"
+  []
+  (notes
+    (concat
+      [[11 2] [16 3] [19 1] [18 2] [16 4] [23 2]]
+      [[21 6] [18 6] [16 3] [19 1] [18 2] [14 4] [17 2] [11 10]]
+      [[11 2] [16 3] [19 1] [18 2] [16 4] [23 2]]
+      [[26 4] [25 2] [24 4] [20 2] [24 3] [23 1] [22 2] [10 4]
+       [19 2] [16 10]])))
+
+(defn ^:export go []
+  (play! woo (magical-theme)))
 
