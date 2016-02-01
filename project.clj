b87ade66574597612109e0951f00072c70011577
Seonho Kim <ksseono@gmail.com>
2016-02-02 00:49:46 +0900

    clojure version update to 1.8

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://www.joyfoclojure.com"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.7.48"]
                  [org.clojure/core.unify "0.5.6"]
                  [org.clojure/core.logic "0.8.10"]
