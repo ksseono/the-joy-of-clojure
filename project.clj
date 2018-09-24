8c6eeb0a9b2de6bbe6b4baa0042893d33a36f736
Seonho Kim <ksseono@gmail.com>
2018-09-24 22:34:45 +0900

    update cemerick/piggieback to cider/piggieback

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.7.228"]
+                 [org.clojure/clojurescript "1.10.339"]
                  [org.clojure/core.unify "0.5.6"]
                  [org.clojure/core.logic "0.8.10"]
-                 [com.cemerick/piggieback "0.2.1"]
+                 [cider/piggieback "0.3.9"]
                  [criterium "0.4.4"]]
   :source-paths ["src/clj/ch01" "src/clj/ch03" "src/clj/ch04" "src/clj/ch05"
                  "src/clj/ch06" "src/clj/ch07" "src/clj/ch08" "src/clj/ch09"
@@ -27,4 +27,4 @@
       :optimizations :advanced
       :externs ["externs.js"]
       :pretty-print false}}]}
-  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]})
+  :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]})
